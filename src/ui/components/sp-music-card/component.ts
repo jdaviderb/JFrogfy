7abd9649f67040be731000094f06e6fcbd782d6b
Jorge Hernandez <jdaviderb@gmail.com>
2019-03-06 10:42:02 -0500

    fix SEO

--- src/ui/components/sp-music-card/component.ts
+++ src/ui/components/sp-music-card/component.ts
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 
 export default class SpMusicCardComponent extends Component {
-  classNames = ['sp-music-card'];
+  classNames: string[] = ['sp-music-card'];
   link: string = 'playlists.show';
 }
--- src/ui/routes/application/controller.ts
+++ src/ui/routes/application/controller.ts
@@ -3,13 +3,16 @@ import { inject as service } from '@ember-decorators/service';
 import { getOwner } from '@ember/application';
 import { computed } from '@ember-decorators/object';
 import TracksManager from 'client/src/services/data/tracks-manager';
+import HeadDataServices from 'client/src/services/head-data';
 
 export default class PlaylistsShowController extends Controller {
   @service('data/tracks-manager') tracksManager: TracksManager;
+  @service headData: HeadDataServices;
 
   @computed
   get headElement() {
     const documentService = getOwner(this).lookup('service:-document');
+
     return documentService.head;
   }
 
